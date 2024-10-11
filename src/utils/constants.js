524b2a26d1dad3c28ad2b7d78abd32a674a579f3
Irfan Parwez <iparwez351412@gmail.com>
2024-10-12 01:08:32 +0530

    env added

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-const API_KEY = "AIzaSyCstvywx-Mg5m9TZO1hdgRFXojna5mANCE";
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 export const YOUTUBE_VIDEO_URL="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+API_KEY;
 export const YOUTUBE_SEARCH_API ="http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
